db699a1766bcd23db4493b01eb824626c1f1631a
Vishal Srivastava <33997413+yudii-vishu@users.noreply.github.com>
2021-08-13 13:01:30 +0530

    Add files via upload

--- DoctorAppointment-API/src/main/java/com/doctorapi/rest/repositories/DoctorDao.java
+++ DoctorAppointment-API/src/main/java/com/doctorapi/rest/repositories/DoctorDao.java
@@ -2,10 +2,12 @@ package com.doctorapi.rest.repositories;
 
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.doctorapi.rest.Enum.Status;
@@ -24,10 +26,18 @@ public interface DoctorDao extends JpaRepository<Doctor, Long> {
 	@Query("SELECT dd from doctor dd where dd.status=?1")
 	public List<Doctor> findByOrderByStatus(Status status);
 
-	public List<Doctor> findByCreatedOn(LocalDateTime localDateTime);
-
 	public List<Doctor> findByModifiedOn(LocalDateTime localDateTime);
 
+	public List<Doctor> getAppointmentById(Long doctorId);
 
+	@Query("SELECT d FROM doctor d WHERE DATE(d.createdOn)=?1")
+	public List<Doctor> findAllWithCreatedOn(@Param("createdOn") Date date1);
+
+	
+	@Query("SELECT d FROM doctor d WHERE DATE(d.modifiedOn)=?1")
+	public List<Doctor> findAllWithModifiedOn(@Param("modifiedOn") Date date1);
+
+
+	
 	
 }
--- DoctorAppointment-API/src/main/java/com/doctorapi/rest/repositories/PatientDao.java
+++ DoctorAppointment-API/src/main/java/com/doctorapi/rest/repositories/PatientDao.java
@@ -1,10 +1,11 @@
 package com.doctorapi.rest.repositories;
 
 
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.doctorapi.rest.models.Patient;
@@ -18,8 +19,12 @@ public interface PatientDao extends JpaRepository<Patient, Long>{
 
 	public Patient findByUserId(Long userId);
 
-	public List<Patient> findByCreatedOn(LocalDateTime createdOn);
+	public  List<Patient> findByAppointments(Long doctorId);
 
-	public List<Patient> findByModifiedOn(LocalDateTime localDateTime);
+	@Query("SELECT p FROM patient p WHERE DATE(p.createdOn)=?1")
+	public List<Patient> findAllWithCreatedOn(Date date1);
+
+	@Query("SELECT p FROM patient p WHERE DATE(p.modifiedOn)=?1")
+	public List<Patient> findAllWithModifiedOn(Date date1);
 
 }
