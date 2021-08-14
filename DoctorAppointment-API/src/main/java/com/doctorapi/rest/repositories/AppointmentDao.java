bd301a4225e7ecc008e5d7913d20890cb3d3b5a1
Vishal Srivastava <33997413+yudii-vishu@users.noreply.github.com>
2021-08-14 14:01:21 +0530

    Add files via upload

--- DoctorAppointment-API/src/main/java/com/doctorapi/rest/repositories/AppointmentDao.java
+++ DoctorAppointment-API/src/main/java/com/doctorapi/rest/repositories/AppointmentDao.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.doctorapi.rest.models.Appointment;
@@ -25,8 +27,10 @@ public interface AppointmentDao extends JpaRepository<Appointment, Long> {
 
 	public List<Appointment> getAppointmentByDoctorId(Long doctorId);
 
-	
+	@Query(value="SELECT * from appointment WHERE doctor_id=?1 and DATE(appointment_date)=CURDATE()", nativeQuery=true)
+	public List<Appointment> findAllAppointmentByDoctorId(@Param("doctor") Long doctorId);
 
+	
 	
 
 }
